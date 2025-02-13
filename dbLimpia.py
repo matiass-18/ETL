b145d8b8691807ee3b242be5cf60aa2f14695f76
Matias Chahin Toledo <matiaschahintoledo18@gmail.com>
2025-02-13 01:23:05 -0500

    Update dbLimpia.py

--- dbLimpia.py
+++ dbLimpia.py
@@ -7,7 +7,7 @@ Created on Wed Feb 12 00:58:58 2025
 
 import pandas as pd
 
-df = pd.read_excel(r"C:\Users\Matias\Desktop\Uni\trabajosU\BigData\tablaOrganizada.xlsx")
+df = pd.read_excel(r"you_route")
 
 df["Fecha de Alquiler"] = df["Fecha de Alquiler"].dt.strftime("%Y-%m-%d")
 df["Fecha de Entrega"] = df["Fecha de Entrega"].dt.strftime("%Y-%m-%d")
