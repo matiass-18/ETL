376a26db2599f4606184e15f53a77ad6aa5b9522
Matias Chahin Toledo <matiaschahintoledo18@gmail.com>
2025-02-13 01:23:43 -0500

    Update dbJson1.py

--- dbJson1.py
+++ dbJson1.py
@@ -7,7 +7,7 @@ Created on Wed Feb 12 00:31:13 2025
 
 import pandas as pd
 
-df = pd.read_excel(r"C:\Users\Matias\Desktop\Uni\trabajosU\BigData\tablaOrganizada.xlsx")
+df = pd.read_excel(r"your_route")
 
 df["Fecha de Alquiler"] = df["Fecha de Alquiler"].dt.strftime("%Y-%m-%d")
 df["Fecha de Entrega"] = df["Fecha de Entrega"].dt.strftime("%Y-%m-%d")
