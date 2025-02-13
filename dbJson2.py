66b7086e7b637dfa112a306fda2f216279eb8c1c
Matias Chahin Toledo <matiaschahintoledo18@gmail.com>
2025-02-13 01:23:26 -0500

    Update dbJson2.py

--- dbJson2.py
+++ dbJson2.py
@@ -7,7 +7,7 @@ Created on Wed Feb 12 01:15:57 2025
 
 import pandas as pd
 
-df = pd.read_excel(r"C:\Users\Matias\Desktop\Uni\trabajosU\BigData\dbLimpia.xlsx")
+df = pd.read_excel(r"your_route")
 
 df["Fecha de Alquiler"] = pd.to_datetime(df["Fecha de Alquiler"], errors="coerce")
 df["Fecha de Entrega"] = pd.to_datetime(df["Fecha de Entrega"], errors="coerce")
